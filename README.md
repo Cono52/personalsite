5af32b075e2cb748663a8edb2904b28e133cd415
Cono52 <oflanac52@gmail.com>
2016-12-12 21:52:45 +0000

    re-initialised structure

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # pportfolio
 Personal Portfolio Site 
-
-Dependencies
-..* hover.min.css
-..* font-awesome.min.css
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , template = require('pug').compileFile(__dirname + '/src/templates/homepage.pug')
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res, next) {
+  try {
+    let html = template({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
