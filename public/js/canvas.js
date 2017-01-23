54bd332d8e25febe1e797e61141db0152eaf3b9c
Cono52 <oflanac52@gmail.com>
2017-01-23 00:39:18 +0000

    jquery pop fix

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,2 +1,12 @@
 let canvas = document.getElementById('top')
 ctx = canvas.getContext("2d")
+
+function calcVH() {
+    $('body').innerHeight( $(this).innerHeight() );
+}
+(function($) { 
+  calcVH();
+  $(window).on('orientationchange', function() {
+    calcVH();
+  });
+})(jQuery);
