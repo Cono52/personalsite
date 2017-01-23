d4d28cd8cf1a0f10024822195345dd9615f1e018
Cono52 <oflanac52@gmail.com>
2017-01-23 00:44:06 +0000

    I rather a pop then a gap under the image

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,12 +1,2 @@
 let canvas = document.getElementById('top')
 ctx = canvas.getContext("2d")
-
-function calcVH() {
-    $('body').innerHeight( $(this).innerHeight() );
-}
-(function($) { 
-  calcVH();
-  $(window).on('orientationchange', function() {
-    calcVH();
-  });
-})(jQuery);
