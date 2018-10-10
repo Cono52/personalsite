bb139019cf5536fd467586511eb724ca01ff5b64
Cono52 <oflanac52@gmail.com>
2018-10-10 14:14:26 +0100

    add dummy nav and other sections

--- src/App.js
+++ src/App.js
@@ -2,21 +2,46 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import HoveringShapeBackground from "./hoveringShape";
 
+const Container = styled.div`
+  position: absolute;
+  top: 2.05rem;
+  right: 2rem;
+  width: 30px;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  margin-bottom: 5px;
+  height: 2px;
+  background-color: #ccc
+`;
+
+const Hamburger = () => (
+  <Container>
+      <Line />
+      <Line />
+      <Line />
+    </Container>
+)
+
+
 const Main = styled.main`
 
 `;
 
-const Home = styled.main`
+const Block = styled.main`
   min-height: 100vh;
   min-width: 100vw;
   position: relative;
+  box-sizing: border-box;
+  padding: ${({ nopad }) => nopad ? '0' : '2rem' };
 `;
 
 const Introduction = styled.div`
-  box-sizing: border-box
   height: 100vh;
-  padding: 2rem;
   display: flex;
+  padding: 2rem;
+  box-sizing: border-box
   flex-direction: column;
   justify-content: space-between;
   font-size: 1rem;
@@ -38,13 +63,20 @@ class App extends Component {
   render() {
     return (
       <Main>
-        <Home>
-        <Introduction>
-          <p>CONOR O'FLANAGAN</p>
-          <p className="tag">Cloud Artisan</p>
-        </Introduction>
-        <HoveringShapeBackground />
-        </Home>
+        <Block nopad>
+          <Introduction>
+            <p>CONOR O'FLANAGAN</p>
+            <p className="tag">Cloud Artisan</p>
+          </Introduction>
+          <Hamburger onClick={() => console.log('openMenu')} />
+          <HoveringShapeBackground />
+        </Block>
+        <Block>
+            About
+        </Block>
+        <Block>
+            Contact
+        </Block>
       </Main>
     );
   }
