447644511ee21cf94def51e4defc459663f6895f
Conor O'Flanagan <oflanac52@gmail.com>
2024-05-06 15:02:24 +0100

    redo website, retire old one, and a draft of an initial article

---
+++ public/old/precache-manifest.8f380b02c615b3435aec10c74dbdf240.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfc6fc91fd4c178cdcc71a16dbe8158",
+    "url": "/old/index.html"
+  },
+  {
+    "revision": "e1720ffaf1731decb3d3",
+    "url": "/old/static/css/2.da08e5a2.chunk.css"
+  },
+  {
+    "revision": "5b1606caa12b4a88d761",
+    "url": "/old/static/css/main.1450324b.chunk.css"
+  },
+  {
+    "revision": "e1720ffaf1731decb3d3",
+    "url": "/old/static/js/2.62feaa75.chunk.js"
+  },
+  {
+    "revision": "0fb1ab9166e4090d40b886200af7baec",
+    "url": "/old/static/js/2.62feaa75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1606caa12b4a88d761",
+    "url": "/old/static/js/main.8aa3ddf8.chunk.js"
+  },
+  {
+    "revision": "4d3a43b973ecacc817d9",
+    "url": "/old/static/js/runtime-main.dd5ae82e.js"
+  },
+  {
+    "revision": "1812b1ad6b0f066a33dd04af7d108f20",
+    "url": "/old/static/media/personal-sitelighthouse.1812b1ad.webp"
+  },
+  {
+    "revision": "a4d6b05d628ef0a84ba7067cefebf246",
+    "url": "/old/static/media/prof.a4d6b05d.webp"
+  }
+]);
